5c2b3e5492aeadf84366280723f3d2e68022d8d3
HenriikRC <hechr16@student.sdu.dk>
2022-09-27 12:54:06 +0200

    Creating Start.java

---
+++ Start.java
@@ -0,0 +1,10 @@
+package worldOfZuul;
+
+public class Start {
+
+    public static void main(String[] args){
+        Game game = new Game();
+        System.out.println(game.getRoomDescription());
+        
+    }
+}
