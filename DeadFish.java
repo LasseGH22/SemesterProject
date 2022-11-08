ab3b44c8269c217575e36fea86d43b13c71bbf51
Lasse Hvilsted <lassehvilsted@gmail.com>
2022-11-08 15:24:53 +0100

    Kommenaterer + Arraylist for årsager i DeadFish

--- DeadFish.java
+++ DeadFish.java
@@ -1,60 +1,45 @@
 package worldOfZuul;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class DeadFish implements Spawnable {
     private String deathReason;
+    private ArrayList<String> reasons = new ArrayList<>();      //Creates an arraylist for the causes of death
 
-    public DeadFish() {
+    public DeadFish() {         //Constructor for the DeadFish class
         this.deathReason = "";
     }
-    public String getDeathReason() {
+    public String getDeathReason() {        //Accesor method for deathReason
         return deathReason;
     }
 
     public void setDeathReason(String deathReason) {
-        this.deathReason = deathReason;
+        this.deathReason = deathReason;     //Mutator method for deathReason
     }
     @Override
-    public boolean spawnChance(){
+    public boolean spawnChance(){       //Calculates the spawnchance of a DeadFish object in a room
         Random random = new Random();
         int chance = random.nextInt(101);
         chance = chance +1;
-        if(chance<101){
+        if(chance<50){
             return true;
         }
         else return false;
     }
 
     @Override
-    public void spawn() {
+    public void spawn() {       //Handles the random picking of a deathreason for the DeadFish object
         Random rng = new Random();
         int random = rng.nextInt(1, 7);
 
-        switch (random) {
-            case 1:
-                setDeathReason("Fisken er viklet ind i en fiskesnor og er druknet");
-                break;
-
-            case 2:
-                setDeathReason("Fisken har fået plastik i spiserøret og er død af sult");
-                break;
-
-            case 3:
-                setDeathReason("Fisken er fanget i et uloveligt fiskenet");
-                break;
-
-            case 4:
-                setDeathReason("Fisken har viklet sig ind i en plasitkpose og er druknet");
-                break;
-
-            case 5:
-                setDeathReason("Fisken har troet plastik var mad og er død af sult");
-                break;
-
-            case 6:
-                setDeathReason("Fisken har noget om kroppen og er blevet for tung til at følge med sin stime");
-                break;
-        }
+        reasons.add("Fisken er viklet ind i en fiskesnor og er druknet");
+        reasons.add("Fisken har fået plastik i spiserøret og er død af sult");
+        reasons.add("Fisken er fanget i et uloveligt fiskenet");
+        reasons.add("Fisken har viklet sig ind i en plasitkpose og er druknet");
+        reasons.add("Fisken har troet plastik var mad og er død af sult");
+        reasons.add("Fisken har noget om kroppen og er blevet for tung til at følge med sin stime");
+        setDeathReason(reasons.get(random));
+        reasons.clear();
     }
     @Override
     public String toString(){
